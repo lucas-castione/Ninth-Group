9664b1d7314cd369b7998109adaf1db034afeae0
Diego Kurman <61751512+diego-kurman@users.noreply.github.com>
2020-05-04 23:35:56 -0300

    adicionando a pasta script

---
+++ script/cadastro.js
@@ -0,0 +1,22 @@
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+})
+
+function logon(){
+   
+    if(input_email.value == 'adm@ninthgroup.com' && input_pass.value == '123456789'){
+        window.location.href ='chartjs-exemplo-uma-linha.html';
+    }else{
+        alert('login ou senha incorretos');
+    }
+}
